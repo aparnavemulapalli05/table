390bed0ede5c31de6887066c5029a8cf2f30e8db
Aparna Vemulapalli <aparnavemulapalli05@gmail.com>
2022-07-27 18:29:07 +0530

    chu chu

---
+++ HashTableA.java
@@ -0,0 +1,13 @@
+package com.advance.map;
+import java.util.Hashtable;
+
+public class HashTableA {
+
+	public static void main(String[] args) {
+		Hashtable<String, Double> productPricesHashTable=new Hashtable<>();
+		 productPricesHashTable.put("Apple", 0.15);
+		 productPricesHashTable.put("orange", 1.15);
+
+	}
+
+}
